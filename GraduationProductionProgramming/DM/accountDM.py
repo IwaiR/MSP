e3353fcd291e3fdbcddbf6f1f2bc0211cc6b290c
Ryoto Iwai <k016c1297@it-neec.jp>
2019-10-29 11:33:54 +0900

    アカウントのControllerとDMできました

--- GraduationProductionProgramming/DM/accountDM.py
+++ GraduationProductionProgramming/DM/accountDM.py
@@ -1,23 +1,117 @@
-# python accountDM.py
-
+from flask import Flask, Blueprint,render_template,redirect,url_for,request, jsonify, make_response
+from urllib.parse import urlparse
 import mysql.connector
 from mysql.connector import Error
-
-# import DB,myCollectionDM
-from DM import DB  # Main.py 実行時
-
-account_ms="account_ms"
+from DM import DB
+#import DB
+#url = urlparse('mysql://k016c1297:kamata@localhost:3306/msp')
+#下記で通信を確立ここから↓
+conn = mysql.connector.connect(
+    host ='localhost',
+    #port = url.port or 3306,
+    user = 'root',
+    password =  'kamata',
+    database = 'msp',
+)
+#conn.is_connected
+#ここまで↑
 account_ms_columns=["accountID","UUID","accountName",
 "specialtyID","profilePhoto_Path","comment"
 ,"specialtyInstrument"]
 
-def getAccountinfoDB(UUID):
-    db=DB.access()
-    db["cur_pre"].execute("""
-    select * from account_ms
-    where UUID=? ;
-    """,(UUID,))
-    result=db["cur_pre"].fetchall()
-    result=DB.Convert_to_dict(account_ms_columns,result)
-    DB.close(db)
+
+#python Main.py
+def CheckAcocountName(accountName):#accountNameはもらってくる値(今回はaccountControllerから)
+#テストOK UUID=004&accountName=IwaiL&specialtyID=1&profilePhoto_Path=IwaiLPhoto.txt&comment=&specialtyInstrument=なし
+    conn = mysql.connector.connect(
+        host ='localhost',
+        #port = url.port or 3306,
+        user = 'root',
+        password =  'kamata',
+        database = 'msp',
+    )
+
+    cur = conn.cursor(prepared=True)#?を使う時　これをdictionary=Trueにするとdictionary型の形で取得できる
+    #入力されたアカウント名の内容が被っていないかの処理↓
+    try:
+        cur.execute('SELECT * from account_MS where accountName =?', (accountName,))
+        #cur.execute('SELECT count(accountName) duplicate_count, accountName from accountName group by account_MS')
+        a = len(cur.fetchall())
+        #b = len(a) #長さでtrueかどうかを分岐させる
+        if a == 0:
+            return True
+        else:
+            return False
+    except Error as e:
+        result=e
+
     return result
+
+
+#DBへのSQL文とContorollerへの記述↓
+def insert(UUID, accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument):
+    cur = conn.cursor(prepared=True)#?を使う時　これをdictionary=Trueにするとdictionary型の形で取得できる
+    try:
+        cur.execute('''INSERT INTO account_MS (UUID, accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument) values (?, ?, ?, ?, ?, ?)'''
+        ,(UUID, accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument))
+        conn.commit()
+        result=True
+    except Error as e:
+        conn.rollback()
+        result=str(e)
+    cur.close()
+    # conn.close()
+    return result
+
+# def getAccountInfoDB(UUID):
+
+def getAccountInfoDB(UUID):
+#テストOK path:http://localhost:5000/accountContoller/getAccountInfo?UUID=001
+    conn = mysql.connector.connect(
+        host ='localhost',
+        #port = url.port or 3306,
+        user = 'root',
+        password =  'kamata',
+        database = 'msp',
+    )
+    #global conn
+
+    cur = conn.cursor(prepared=True)#?を使う時　これをdictionary=Trueにするとdictionary型の形で取得できる
+    try:
+        cur.execute('SELECT * from account_MS where UUID=?', (UUID,))
+        #conn.commit() selectはcommitいらない
+        result=cur.fetchall()
+        result=DB.Convert_to_dict(account_ms_columns, result)
+        #account_ms_columns(上記)をresultにいえる account_ms_columns内の全てのデータがないと成り立たない
+    except:
+        #conn.rollback()
+        result=False
+    conn.close()
+    return result
+
+#updateはTrue or False の形で返す
+def updateAccountInfoDB(accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument, UUID):
+    conn = mysql.connector.connect(
+        host ='localhost',
+        #port = url.port or 3306,
+        user = 'root',
+        password =  'kamata',
+        database = 'msp',
+    )
+
+    cur = conn.cursor(prepared=True)#?を使う時　これをdictionary=Trueにするとdictionary型の形で取得できる
+    try:
+        cur.execute('''update account_MS set accountName= ?, specialtyID=?, profilePhoto_Path=?,
+        comment=?, specialtyInstrument=? where UUID= ? ''', (accountName, specialtyID, profilePhoto_Path, comment, specialtyInstrument, UUID))
+        conn.commit()
+        result=True
+    except Error as e:
+        print("error",e)
+        conn.rollback()
+        result=False
+    conn.close()
+    return result
+
+#print(getAccountInfoDB('00000000000000000000000000000001'))
+#DMはBlueprintをいれる必要なし
+#呼ばれる関数を記述する
--- GraduationProductionProgramming/func1/accountController.py
+++ GraduationProductionProgramming/func1/accountController.py
@@ -1,20 +1,103 @@
-from flask import Flask, request, jsonify, make_response
-app = Flask(__name__)
+from flask import Flask, Blueprint,render_template,redirect,url_for, request, jsonify, make_response
+import mysql.connector
+# from ..DM import accountDM
+#from ..DM
+#import sys
+#sys.path.append('../')
+from DM import accountDM
+#import C:\Users\k016c1297\Desktop\GIT2\GraduationProductionProgramming_sample\DM\accountDM
+#import 呼ぶファイル名
+app = Blueprint('accountContoller', __name__)
 
-@app.route("/hoge", methods=['GET'])
-def getHoge():
-    # URLパラメータ
+@app.route('/accountContoller/registration', methods=['GET', 'POST'])#ファイル名+関数名
+def registration():
+    #APIを使ったレスポンス↓
+    #with app.test_request_context():
     params = request.args
     response = {}
-    for param in params:
-        response.setdefault(param,params[param])
-    return make_response(jsonify(response))
+    try: #accountNameのパラメーターがなければtry文でexceptにいくここかないとパラメーターに入力がなくても実行されてしまう
+        accountName = params["accountName"]#accountNameパラメーターがある前提にする
 
-@app.route("/hoge",methods=["POST"])
-def postHoge():
-    # ボディ(application/json)パラメータ
-    params = request.json
+        result=accountDM.CheckAcocountName(accountName)
+        if result == True:
+            UUID=params["UUID"]
+            # accountName
+            response=accountDM.insert(params["UUID"], params['accountName'], params['specialtyID'], params['profilePhoto_Path'], params['comment'], params['specialtyInstrument'])
+        #response.setdefault('res', 'UUID : ' + params.get('UUID') + ' accountName : '+ params.get('accountName') + ' specialtyID :  ' + params.get('specialtyID') + ' profilePhoto_Path : ' + params.get('profilePhoto_Path') + ' comment : ' + params.get('comment') + ' specialtyInstrument : ' + params.get('specialtyInstrument'))
+    # body = { "param" : "accountContoller"}
+    # response = make_response(jsonify(body))
+    # response.headers['registration'] = 'accountContoller...'
+        else:
+            response=False
+
+        return make_response(jsonify(response))
+    except:
+        return "False"
+
+
+    #accountDM.UUID=params["UUID"]
+    # accountName
+    #/accountContoller/registrationの後、?UUID=0001&accountName="Iwai"...とパラメーターを書く
+    #if 'UUID' in params and 'accountName' in params and 'specialtyID' in params and 'profilePhoto_Path' in params and 'comment' in params and 'specialtyInstrument' in params:
+
+
+
+    # accountDM.insert(UUID, accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument)
+
+    #テスト用パラメーター↓
+    # UUID = "00000000000000000000000000000002"#htmlとつなぐ前にテスト
+    # accountName = "IwaiR"
+    # specialtyID = "4"
+    # profilePhoto_Path = "https://IwaiR_Photo.txt"
+    # comment = "よろしくお願いします！"
+    # specialtyInstrument = "ギター"
+    # accountDM.insert(UUID, accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument)#URLのパラメーターを想定して
+
+@app.route('/accountContoller/getAccountInfo', methods=['GET', 'POST'])#ファイル名+関数名
+#テストOK path:http://localhost:5000/accountContoller/getAccountInfo?UUID=001
+def getAccountInfo():#tryほしい
     response = {}
-    for param in params:
-        response.setdefault(param,params[param])
-    return make_response(jsonify(response))
+    params = request.args
+    try:
+        UUID=params["UUID"]
+        accountDM.getAccountInfoDB(UUID)
+        result = accountDM.getAccountInfoDB(UUID)
+        response = result
+        #print(accountDM.getAccountInfoDB(UUID))
+        return make_response(jsonify(response))
+    except:
+        return False
+
+@app.route('/accountContoller/updateAccountInfo', methods=['GET', 'POST'])#ファイル名+関数名
+#テストOK Path:http://localhost:5000/accountContoller/updateAccountInfo?accountName=IwaiR&specialtyID=3&profilePhoto_Path=IwaiNPhoto.txt&comment=&specialtyInstrument=&UUID=001
+def updateAccountInfo():#UUIDもわたす　print(result)ほしい
+    params = request.args
+    response = {}
+    try:
+        accountName = params["accountName"]
+        specialtyID = params["specialtyID"]
+        profilePhoto_Path = params["profilePhoto_Path"]
+        comment = params["comment"]
+        specialtyInstrument = params["specialtyInstrument"]
+        UUID = params["UUID"]
+
+        #accountDM.updateAccountInfoDB(accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument, UUID)
+        # if 'accountName' in params and 'specialtyID' in params and 'profilePhoto_Path' in params and 'comment' in params and 'specialtyInstrument' in params and 'UUID' in params:
+        #      response.setdefault('res','accountName : '+ params.get('accountName') + 'specialtyID :  ' + params.get('specialtyID') + 'profilePhoto_Path : ' + params.get('profilePhoto_Path') + 'comment : ' + params.get('comment') + 'specialtyInstrument : ' + params.get('specialtyInstrument') + 'UUID : ' + params.get('UUID'))
+        result = accountDM.updateAccountInfoDB(accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument, UUID)
+        response = result
+        return make_response(jsonify(response))
+    except:
+        return "False"
+
+    # accountName = "IwaiL"
+    # specialtyID = "1"
+    # profilePhoto_Path = "https://IwaiL_Photo.txt"
+    # comment = "実は楽器ができない"
+    # specialtyInstrument = "なし"
+    # UUID = "00000000000000000000000000000001"#htmlとつなぐ前にテスト
+
+    #accountDM.updateAccountInfoDB(accountName,  specialtyID, profilePhoto_Path, comment, specialtyInstrument)
+    #引数にUUIDをもってきてほしい
+    #returnで返ってきて画面にreturnする
+#UUID=001&accountName="IwaiR"&specialtyID="1"&profilePhoto_Path="IwaiPhoto.txt"&comment="よろしく"&specialtyInstrument="なし"
